bf3df1bdea287265e50a21e2f7a530e86437f35a
violletta1 <villygeorgieva@yahoo.com>
2023-12-16 12:34:09 +0000

    add ui test and change the url in my ui tests

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('http://localhost:8080/shopping-list');
+    await page.goto('https://violetta1-exam.onrender.com/Shopping-List');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
